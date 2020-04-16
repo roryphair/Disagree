cacc327b81dac245b3f87dab0e998e22060f3518
Rory Phair <roryphair@gmail.com>
2020-04-16 16:58:52 -0400

    fixed message bug

--- app/controllers/api/direct_messages_controller.rb
+++ app/controllers/api/direct_messages_controller.rb
@@ -3,22 +3,26 @@ class Api::DirectMessagesController < ApplicationController
         @direct_message = DirectMessage.new(direct_message_params);
         @direct_message.author_id = current_user.id
         @direct_message.receiver_id = params[:user_id]
-        if @direct_message.save
-            @channel_id = channelLogic(@direct_message.author_id, @direct_message.receiver_id)
-            new_message = {
-                message: {
-                    id: @direct_message.id,
-                    user_id: @direct_message.author_id,
-                    body: @direct_message.body,
-                    created_at: @direct_message.created_at,
-                    updated_at:  @direct_message.updated_at.strftime("%m/%d/%Y %I:%M%p"),
-                },
-                channelId: @channel_id,
-            }
-            ChannelMessagesChannel.speak(@channel_id , new_message.as_json)
-            render :show
+        if @direct_message.receiver_id == @direct_message.author_id
+            render json: ["Sorry you can't talk to yourself"], status: 403
         else
-            render json: @direct_message.errors.full_messages, status: 404
+            if @direct_message.save
+                @channel_id = channelLogic(@direct_message.author_id, @direct_message.receiver_id)
+                new_message = {
+                    message: {
+                        id: @direct_message.id,
+                        user_id: @direct_message.author_id,
+                        body: @direct_message.body,
+                        created_at: @direct_message.created_at,
+                        updated_at:  @direct_message.updated_at.strftime("%m/%d/%Y %I:%M%p"),
+                    },
+                    channelId: @channel_id,
+                }
+                ChannelMessagesChannel.speak(@channel_id , new_message.as_json)
+                render :show
+            else
+                render json: @direct_message.errors.full_messages, status: 404
+            end
         end
     end
 
@@ -33,15 +37,20 @@ class Api::DirectMessagesController < ApplicationController
     def create_first
         @user = User.find_by(username: params[:message][:username])
         if @user
-            @direct_message = DirectMessage.new(direct_message_params);
-            @direct_message.author_id = current_user.id
-            @direct_message.receiver_id = @user.id
-            if @direct_message.save
-                @channel_id = channelLogic(@direct_message.author_id, @direct_message.receiver_id)
-                render :show
+            if @user.id == current_user.id
+                render json: ["Sorry you can't talk to yourself"], status: 403
             else
-                render json: @direct_message.errors.full_messages, status: 404
+                @direct_message = DirectMessage.new(direct_message_params);
+                @direct_message.author_id = current_user.id
+                @direct_message.receiver_id = @user.id
+                if @direct_message.save
+                    @channel_id = channelLogic(@direct_message.author_id, @direct_message.receiver_id)
+                    render :show
+                else
+                    render json: @direct_message.errors.full_messages, status: 404
+                end
             end
+            
         else
             render json: ['That user does not exist'], status: 404
         end
